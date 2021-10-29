1759004e4aaf6904f2340a8267e48dfc8120bb23
SanB <sandro.brignoli@outlook.fr>
2021-10-29 11:02:20 +0200

    ⚡️ Update Slider Component

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -7,19 +7,19 @@ const Slider = () => {
     {
       id: 1,
       image: `${baseUrl}1.jpeg`,
-      title: `Slider 1`,
+      title: `Partenaire n°1`,
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur nobis unde quisquam cumque culpa facilis fugit error aliquid dolores, ex dicta quia aut at in repudiandae dolorem, omnis quibusdam laudantium.",
     },
     {
       id: 2,
       image: `${baseUrl}2.jpeg`,
-      title: `Slider 2`,
+      title: `Partenaire n°2`,
       text: "Consequuntur nobis unde quisquam cumque culpa facilis fugit error aliquid dolores, ex dicta quia aut at in repudiandae dolorem, omnis quibusdam laudantium. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     },
     {
       id: 3,
       image: `${baseUrl}3.jpeg`,
-      title: `Slider 3`,
+      title: `Partenaire n°3`,
       text: "Omnis quibusdam laudantium, lorem ipsum dolor sit amet consectetur adipisicing elit. Ex dicta quia aut at in repudiandae dolorem, Consequuntur nobis unde quisquam cumque culpa facilis fugit error aliquid dolores.",
     },
   ];
@@ -29,25 +29,22 @@ const Slider = () => {
         <div className='slider__title'>
           <h2>Nos Partenaires</h2>
         </div>
-        {/* <div className='slider__carousel'>
+        <div className='slider__carousel'>
           <Carousel
             autoPlay
-            interval={6000}
+            interval={2500}
             infiniteLoop
             showThumbs={false}
             showStatus={false}
           >
             {data.map((slide) => (
-              <div key={slide.id} className=''>
-                <img src={slide.image} alt={slide.title} />
-                <div className='overlay'>
-                  <h2 className='overlay__title'>{slide.title}</h2>
-                  <p className='overlay__text'>{slide.text}</p>
-                </div>
+              <div key={slide.id}>
+                <h2 className='slide__title'>{slide.title}</h2>
+                <p className='slide__text'>{slide.text}</p>
               </div>
             ))}
           </Carousel>
-        </div> */}
+        </div>
       </div>
     </section>
   );
