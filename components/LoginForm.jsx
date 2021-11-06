b83c6bb6148c3a5694671b6fae87241c34a13258
SanB <sandro.brignoli@outlook.fr>
2021-11-06 18:05:13 +0100

    ✨ Create Login & loadUser

---
+++ components/LoginForm.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../actions/auth";
+
+import Input from "../components/common/Input";
+
+const LoginForm = () => {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector(
+    (state) => state.authReducer.isAuthenticated
+  );
+  const router = useRouter();
+
+  const [formData, setformData] = useState({
+    mail: "",
+    password: "",
+  });
+  const { mail, password } = formData;
+
+  const onChange = (e) => {
+    setformData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login({ mail, password }));
+    document.getElementById("password").value = "";
+  };
+
+  isAuthenticated && router.push("/");
+
+  return (
+    <form id='loginForm' onSubmit={(e) => onSubmit(e)}>
+      <Input
+        name='mail'
+        label='Adresse e-mail'
+        placeholder='Votre adresse e-mail'
+        type='email'
+        onChange={(e) => onChange(e)}
+      />
+      <Input
+        name='password'
+        label='Mot de passe'
+        placeholder='Votre mot de passe'
+        type='password'
+        id='password'
+        onChange={(e) => onChange(e)}
+      />
+      <Input type='submit' value='Se connecter' />
+    </form>
+  );
+};
+
+export default LoginForm;
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
 import { Provider } from "react-redux";
+import { useEffect } from "react";
 
+import { loadUser } from "../actions/auth";
+import setAuthToken from "../utils/setAuthToken";
 import store from "../store";
 import "../styles.scss";
 
 function MyApp({ Component, pageProps }) {
+  if (typeof window !== "undefined") {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+  }
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Provider store={store}>
       <Component {...pageProps} />
