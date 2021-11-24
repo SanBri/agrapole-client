67096899c4e3b10e0fa28d5ce56da4eaf4a6906e
SanB <sandro.brignoli@outlook.fr>
2021-11-24 16:40:13 +0100

    ✨ Update editUser Action

--- actions/user.js
+++ actions/user.js
@@ -1,23 +1,32 @@
 import axios from "axios";
 
 import { setAlert } from "./alert";
+import { loadUser } from "./auth";
 
-// Edit Password
-export const editPassword = (formData, id, blockID) => async (dispatch) => {
+// Edit Password or Mail
+export const editUser = (type, formData, id, blockID) => async (dispatch) => {
   try {
     const config = {
       headers: {
         "Content-Type": "application/json",
       },
     };
-    await axios.put(
-      `${process.env.URL}/users/password/${id}`,
-      formData,
-      config
-    );
-    dispatch(
-      setAlert("Le mot de passe a bien été modifié", "success", blockID)
-    );
+    if (type === "editPassword") {
+      await axios.put(
+        `${process.env.URL}/users/password/${id}`,
+        formData,
+        config
+      );
+      dispatch(
+        setAlert("Le mot de passe a bien été modifié", "success", blockID)
+      );
+    } else if (type === "editMail") {
+      await axios.put(`${process.env.URL}/users/mail/${id}`, formData, config);
+      dispatch(
+        setAlert("L'adresse mail bien été modifiée", "success", blockID)
+      );
+      dispatch(loadUser()); // To update the mail in the state/store
+    }
     return true;
   } catch (err) {
     const errors = err.response.data.errors;
