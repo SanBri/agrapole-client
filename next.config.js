c4ee5494c1747cc7281af5b07597e6ff85eab957
SanB <sandro.brignoli@outlook.fr>
2021-12-12 20:09:04 +0100

    🧪 Test Environment Variables

--- next.config.js
+++ next.config.js
@@ -3,5 +3,7 @@ module.exports = {
   env: {
     // URL: "http://localhost:5000/api",
     URL: "https://frseaaura.herokuapp.com/api",
+    USER_ID: "@USER_ID",
+    TEMPLATE_ID: "@TEMPLATE_ID",
   },
 };
