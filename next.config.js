ce679ee2460a5496d488619bc8874d3bea76f6fa
SanB <sandro.brignoli@outlook.fr>
2022-11-23 20:35:28 +0100

    🚀 Linked to Render Backend

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ module.exports = {
   reactStrictMode: true,
   env: {
     // URL: "http://localhost:5000/api",
-    URL: "https://frseaaura.herokuapp.com/api",
+    // URL: "https://frseaaura.herokuapp.com/api",
+    URL: "https://frsea-aura.onrender.com/api",
     USER_ID: process.env.USER_ID,
     TEMPLATE_ID: process.env.TEMPLATE_ID,
   },
